68483626f16dad213ee0a24e943058949a7659a7
MIYAKOSHI Yuki <miyakoshi.yuki@raz.jp>
2025-02-23 15:22:18 +0900

    20250223

--- hugo_server.ps1
+++ hugo_server.ps1
@@ -2,7 +2,7 @@
 # PowerShellスクリプト
 
 # 指定したディレクトリに移動
-Set-Location -Path "C:\Users\miyak\OneDrive\ドキュメント\GitHub\raz.jp"
+Set-Location -Path "C:\Users\miyak\OneDrive\ドキュメント\GitHub\www.miyakoshiyuki.jp"
 
 # Hugo サーバーを起動
 hugo server
